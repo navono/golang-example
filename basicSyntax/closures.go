6553471a3d328330238df2b2d8f95bbb4c19afe4
navono007 <navono007@gmail.com>
2017-11-02 20:56:41 +0800

    :sparkles: closure

---
+++ basicSyntax/closures.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 闭包是一个函数值，它引用了函数体之外的变量。
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
