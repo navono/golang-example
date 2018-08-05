407a3eb78aa2c3747dbae4f3171601f5aecc1c31
pingqixing <navono007@gmail.com>
2018-08-05 17:19:12 +0800

    add: context

--- pattern/greeter-context.go
+++ pattern/greeter-context.go
@@ -54,6 +54,11 @@ func printFarewell(ctx context.Context) error {
 }
 
 func locale(ctx context.Context) (string, error) {
+	if deadline, ok := ctx.Deadline(); ok {
+		if deadline.Sub(time.Now().Add(1*time.Minute)) <= 0 {
+			return "", context.DeadlineExceeded
+		}
+	}
 	select {
 	case <-ctx.Done():
 		return "", ctx.Err()
