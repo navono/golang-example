f4ff64cb01bb27858ffdadecabe815d5d399e566
navono <navono007@gmail.com>
2018-08-11 14:29:26 +0800

    wrapp error

--- error-handle/intermediate.go
+++ error-handle/intermediate.go
@@ -12,9 +12,13 @@ func RunJob(id string) error {
 	const jobBinPath = "/bad/job/binary"
 	isExecutable, err := isGloballyExec(jobBinPath)
 	if err != nil {
-		return err
+		return IntermediateErr{wrapError(
+			err,
+			"cannot run job %q: requisite binaries not available",
+			id,
+		)}
 	} else if isExecutable == false {
-		return wrapError(nil, "job binary is not executable")
+		return wrapError(nil, "job binary is not executable", id)
 	}
 	return exec.Command(jobBinPath, "--id="+id).Run()
 }
