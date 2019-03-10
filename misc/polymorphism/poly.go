8fb3925edb6483960823aaa3d4375c67f8401834
navono <navono007@gmail.com>
2019-03-10 17:06:24 +0800

    add another struct to polymorphism

--- misc/polymorphism/poly.go
+++ misc/polymorphism/poly.go
@@ -16,13 +16,6 @@ type FixedBilling struct {
 	biddedAmount int
 }
 
-// TimeAndMaterial represent another project
-type TimeAndMaterial struct {
-	projectName string
-	noOfHours   int
-	hourlyRate  int
-}
-
 func (fb FixedBilling) calculate() int {
 	return fb.biddedAmount
 }
@@ -31,12 +24,34 @@ func (fb FixedBilling) source() string {
 	return fb.projectName
 }
 
+// TimeAndMaterial represent another project
+type TimeAndMaterial struct {
+	projectName string
+	noOfHours   int
+	hourlyRate  int
+}
+
 func (tm TimeAndMaterial) calculate() int {
 	return tm.noOfHours * tm.hourlyRate
 }
 
 func (tm TimeAndMaterial) source() string {
 	return tm.projectName
+}
+
+// Advertisement for third project
+type Advertisement struct {
+	adName     string
+	CPC        int
+	noOfClicks int
+}
+
+func (a Advertisement) calculate() int {
+	return a.CPC * a.noOfClicks
+}
+
+func (a Advertisement) source() string {
+	return a.adName
 }
 
 func calculateNetIncome(ic []Income) {
@@ -55,7 +70,11 @@ func init() {
 	project1 := FixedBilling{projectName: "Project 1", biddedAmount: 5000}
 	project2 := FixedBilling{projectName: "Project 2", biddedAmount: 10000}
 	project3 := TimeAndMaterial{projectName: "Project 3", noOfHours: 160, hourlyRate: 25}
-	incomeStreams := []Income{project1, project2, project3}
+
+	bannerAd := Advertisement{adName: "Banner Ad", CPC: 2, noOfClicks: 500}
+	popupAd := Advertisement{adName: "Popup Ad", CPC: 5, noOfClicks: 750}
+
+	incomeStreams := []Income{project1, project2, project3, bannerAd, popupAd}
 	calculateNetIncome(incomeStreams)
 
 	fmt.Println("<=== exit polymorphism package")
