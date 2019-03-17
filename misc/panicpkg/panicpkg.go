54aa7398c9641f788c8ba61ccbac912eeedb4517
navono <navono007@gmail.com>
2019-03-17 12:52:41 +0800

    add panic

--- misc/panicpkg/panicpkg.go
+++ misc/panicpkg/panicpkg.go
@@ -1,6 +1,9 @@
 package panicpkg
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 // 使用 panic 的场景
 // 1. 程序发生了不可恢复的故障，同时又不能简单地继续执行下去
@@ -9,7 +12,8 @@ import "fmt"
 //    比如一个指针类型的参数接收了一个 nil 的实参。
 
 func fullName(firstName *string, lastName *string) {
-	defer fmt.Println("deferred call in fullName")
+	// defer fmt.Println("deferred call in fullName")
+	defer recoverName()
 
 	if firstName == nil {
 		panic("runtime error: first name cannot be nil")
@@ -19,6 +23,17 @@ func fullName(firstName *string, lastName *string) {
 	}
 	fmt.Printf("%s %s\n", *firstName, *lastName)
 	fmt.Println("returned normally from fullName")
+}
+
+// Recover works only when it is called from the same goroutine.
+// recover 也可以使用在运行时错误中，比如越界索引
+func recoverName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from", r)
+
+		// 增加输出栈信息
+		debug.PrintStack()
+	}
 }
 
 func init() {
