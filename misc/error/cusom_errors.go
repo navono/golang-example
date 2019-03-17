707ac40cad07fff8ab840dd9b6b38ceda94f68e0
navono <navono007@gmail.com>
2019-03-17 11:17:29 +0800

    add struct errors

--- misc/error/cusom_errors.go
+++ misc/error/cusom_errors.go
@@ -28,32 +28,81 @@ func testCustomError() {
 
 // user struct type
 type areaError struct {
-	err    string
-	radius float64
+	err    string  //error description
+	length float64 //length which caused the error
+	width  float64 //width which caused the error
 }
+
+// old version
+// func circleAreaWithTypeError(radius float64) (float64, error) {
+// 	if radius < 0 {
+// 		return 0, &areaError{"radius is negative", radius}
+// 	}
+// 	return math.Pi * radius * radius, nil
+// }
+
+// func testTypedError() {
+
+// 	radius := -20.0
+// 	area, err := circleAreaWithTypeError(radius)
+// 	if err != nil {
+// 		if err, ok := err.(*areaError); ok {
+// 			fmt.Printf("Radius %0.2f is less than zero\n", err.radius)
+// 			return
+// 		}
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Printf("Area of rectangle1 %0.2f\n", area)
+// }
 
 func (e *areaError) Error() string {
-	return fmt.Sprintf("radius %0.2f: %s\n", e.radius, e.err)
+	return e.err
 }
 
-func circleAreaWithTypeError(radius float64) (float64, error) {
-	if radius < 0 {
-		return 0, &areaError{"radius is negative", radius}
+func (e *areaError) lengthNegative() bool {
+	return e.length < 0
+}
+
+func (e *areaError) widthNegative() bool {
+	return e.width < 0
+}
+
+func rectArea(length, width float64) (float64, error) {
+	err := ""
+	if length < 0 {
+		err += "length is less than zero"
 	}
-	return math.Pi * radius * radius, nil
+	if width < 0 {
+		if err == "" {
+			err = "width is less than zero"
+		} else {
+			err += ", width is less than zero"
+		}
+	}
+	if err != "" {
+		return 0, &areaError{err, length, width}
+	}
+	return length * width, nil
 }
 
 func testTypedError() {
-
-	radius := -20.0
-	area, err := circleAreaWithTypeError(radius)
+	length, width := -5.0, -9.0
+	area, err := rectArea(length, width)
 	if err != nil {
 		if err, ok := err.(*areaError); ok {
-			fmt.Printf("Radius %0.2f is less than zero\n", err.radius)
+			if err.lengthNegative() {
+				fmt.Printf("error: length %0.2f is less than zero\n", err.length)
+
+			}
+			if err.widthNegative() {
+				fmt.Printf("error: width %0.2f is less than zero\n", err.width)
+
+			}
 			return
 		}
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Area of rectangle1 %0.2f\n", area)
+	fmt.Println("area of rect", area)
 }
