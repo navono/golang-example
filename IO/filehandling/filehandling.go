0a29f294c319003cc44d2f5dcb0cdf3ff7c0ef82
navono <navono007@gmail.com>
2019-03-24 13:41:45 +0800

    add file reading

--- IO/filehandling/filehandling.go
+++ IO/filehandling/filehandling.go
@@ -1,8 +1,10 @@
 package filehandling
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
@@ -16,11 +18,61 @@ func testReadRelativeFile() {
 	fmt.Println("Contents of file:", string(data))
 }
 
+func testBufferedRead() {
+	pwd, _ := os.Getwd()
+	f, err := os.Open(pwd + "\\IO\\test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r := bufio.NewReader(f)
+	b := make([]byte, 3)
+	for {
+		_, err := r.Read(b)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			break
+		}
+		fmt.Println(string(b))
+	}
+}
+
+func testReadFileLineByLine() {
+	pwd, _ := os.Getwd()
+	f, err := os.Open(pwd + "\\IO\\test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
+	err = s.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func init() {
 	fmt.Println()
 	fmt.Println("===> enter IO package")
 
-	testReadRelativeFile()
+	// testReadRelativeFile()
+	// testBufferedRead()
+	testReadFileLineByLine()
 
 	fmt.Println("<=== exit IO package")
 	fmt.Println()
