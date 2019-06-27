369f5037532e254c4e76989077b34f679b503e2b
navono <navono007@gmail.com>
2019-06-27 16:03:05 +0800

    fix: 删除注释代码

--- misc/ws/proxy/main.go
+++ misc/ws/proxy/main.go
@@ -19,27 +19,5 @@ func main() {
 	}
 
 	log.Println("proxy server running on localhost:9090")
-	err = http.ListenAndServe(":9090", websocketproxy.NewProxy(u))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//listener, err := net.Listen("tcp", ":8080")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//for {
-	//	conn, err := listener.Accept()
-	//	if err != nil {
-	//		//handle error.
-	//		log.Fatalf("failed listening for '%v' on %v: %v", "localhost", 8080, err)
-	//	}
-	//	go handleConn(conn)
-	//}
+	log.Fatalln(http.ListenAndServe(":9090", websocketproxy.NewProxy(u)))
 }
-
-//
-//func handleConn(from net.Conn) {
-//
-//}
