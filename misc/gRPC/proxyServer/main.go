d6ae82917494c8e981ab2ecddd298516c3813bfa
navono <navono007@gmail.com>
2019-06-28 09:43:44 +0800

    feat: 增加 ectd/raft 的例子

--- misc/gRPC/proxyServer/main.go
+++ misc/gRPC/proxyServer/main.go
@@ -8,34 +8,62 @@ import (
 	"github.com/mwitkow/grpc-proxy/proxy"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
+	pb "golang-example/misc/gRPC/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"strings"
 )
 
 const (
 	port = "9000"
 )
 
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name + " from proxy server"}, nil
+}
+
 func main() {
 	pflag.Parse()
 	logrus.SetOutput(os.Stdout)
 	logEntry := logrus.NewEntry(logrus.StandardLogger())
 
-	proxyServer := buildGrpcProxyServer(logEntry)
+	proxyServer := buildGRPCProxyServer(logEntry)
 
 	listen, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Fatalf("failed to Listen: %v", err)
 	}
 
+	pb.RegisterGreeterServer(proxyServer, &server{})
+
+	// graceful shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			// sig is a ^C, handle it
+			log.Println("shutting down gRPC server...")
+
+			proxyServer.GracefulStop()
+		}
+	}()
+
 	log.Println("starting proxy server, listening on 9000... ")
 	log.Fatal(proxyServer.Serve(listen))
 }
 
-func buildGrpcProxyServer(logger *logrus.Entry) *grpc.Server {
+func buildGRPCProxyServer(logger *logrus.Entry) *grpc.Server {
 	// gRPC-wide changes.
 	grpc.EnableTracing = true
 	grpc_logrus.ReplaceGrpcLogger(logger)
@@ -43,11 +71,17 @@ func buildGrpcProxyServer(logger *logrus.Entry) *grpc.Server {
 	// gRPC proxy logic.
 	backendConn := dialBackendOrFail()
 	director := func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+		if !strings.HasPrefix(fullMethodName, "/helloworld.Greeter/SayHello") {
+			return nil, nil, status.Errorf(codes.Unimplemented, "Unknown method")
+		}
+
 		md, _ := metadata.FromIncomingContext(ctx)
-		outCtx, _ := context.WithCancel(ctx)
-		mdCopy := md.Copy()
-		delete(mdCopy, "user-agent")
-		outCtx = metadata.NewOutgoingContext(outCtx, mdCopy)
+		//outCtx, _ := context.WithCancel(ctx)
+		//mdCopy := md.Copy()
+		//delete(mdCopy, "user-agent")
+		//outCtx = metadata.NewOutgoingContext(outCtx, mdCopy)
+
+		outCtx := metadata.NewOutgoingContext(ctx, md.Copy())
 		return outCtx, backendConn, nil
 	}
 	// Server with logging and monitoring enabled.
@@ -55,6 +89,7 @@ func buildGrpcProxyServer(logger *logrus.Entry) *grpc.Server {
 		grpc.CustomCodec(proxy.Codec()), // needed for proxy to function.
 		grpc.UnknownServiceHandler(proxy.TransparentHandler(director)),
 		grpc_middleware.WithUnaryServerChain(
+			methodInterceptor,
 			grpc_logrus.UnaryServerInterceptor(logger),
 			grpc_prometheus.UnaryServerInterceptor,
 		),
@@ -63,4 +98,11 @@ func buildGrpcProxyServer(logger *logrus.Entry) *grpc.Server {
 			grpc_prometheus.StreamServerInterceptor,
 		),
 	)
+}
+
+func methodInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Printf("gRPC method: %s, %v", info.FullMethod, req)
+	resp, err := handler(ctx, req)
+	log.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	return resp, err
 }
