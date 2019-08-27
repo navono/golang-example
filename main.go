e96d171224a8a361cb7528ba973c7decc1276b31
pingqixing <pingqixing@supcon.com>
2019-08-27 11:06:13 +0800

    refactor: 重构

--- main.go
+++ main.go
@@ -1,33 +1,6 @@
 package main
 
-import (
-	"fmt"
-	// _ "golang-example/lang-related/basic/variables"
-	// _ "golang-example/lang-related/basic/types"
-	// _ "golang-example/lang-related/basic/pointer"
-	// _ "golang-example/lang-related/basic/arraySlices"
-	// _ "golang-example/lang-related/basic/functions"
-	// _ "golang-example/lang-related/basic/conditional"
-	// _ "golang-example/lang-related/basic/structure"
-	// _ "golang-example/lang-related/basic/methods"
-	// _ "golang-example/lang-related/interface"
-	// _ "golang-example/lang-related/concurrency"
-	// _ "golang-example/lang-related/misc/oop"
-	// _ "golang-example/lang-related/misc/composition"
-	// _ "golang-example/lang-related/misc/polymorphism"
-	// _ "golang-example/lang-related/misc/deferpkg"
-	// _ "golang-example/lang-related/misc/errorpkg"
-	// _ "golang-example/lang-related/misc/panicpkg"
-	// _ "golang-example/lang-related/misc/firstclassfunction"
-	//_ "golang-example/lang-related/misc/reflection"
-	//_ "golang-example/lang-related/dump"
-	//_ "golang-example/misc/gops"
-	//_ "golang-example/misc/badger"
-	//_ "golang-example/misc/casbin"
-	//_ "golang-example/misc/authentication/session-based"
-	//_ "golang-example/misc/log"
-	_ "golang-example/misc/wasm"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("main exit")
