3d8513e8868ead5eba3df4f0ca8fcc439bd670d0
navono <navono007@gmail.com>
2019-10-24 10:55:46 +0800

    fix: gorm 单元测试例子修改

--- misc/go-sqlmock/repository/person_test.go
+++ misc/go-sqlmock/repository/person_test.go
@@ -2,13 +2,14 @@ package repository
 
 import (
 	"database/sql"
+	"database/sql/driver"
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/go-test/deep"
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
@@ -33,7 +34,8 @@ func (s *Suite) SetupSuite() {
 	db, s.mock, err = sqlmock.New()
 	require.NoError(s.T(), err)
 
-	s.DB, err = gorm.Open("postgres", db)
+	// https://github.com/jirfag/go-queryset/blob/master/internal/queryset/generator/queryset_test.go
+	s.DB, err = gorm.Open("sqlite3", db)
 	require.NoError(s.T(), err)
 
 	s.DB.LogMode(true)
@@ -50,28 +52,30 @@ func TestInit(t *testing.T) {
 	suite.Run(t, new(Suite))
 }
 
-func (s *Suite) Test_repository_Get() {
-	var (
-		id   = uuid.NewV4()
-		name = "test-name"
-	)
-
-	s.mock.MatchExpectationsInOrder(false)
-	s.mock.ExpectQuery(regexp.QuoteMeta(
-		`SELECT * FROM "person" WHERE (id = $1)`)).
-		WithArgs(id.String()).
-		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(id.String(), name))
-
-	res, err := s.repository.Get(id)
-
-	require.NoError(s.T(), err)
-	require.Nil(s.T(), deep.Equal(&model.Person{ID: id, Name: name}, res))
-
-	// // we make sure that all expectations were met
-	// if err := s.mock.ExpectationsWereMet(); err != nil {
-	// 	require.NoError(s.T(), err)
-	// }
-}
+//
+// func (s *Suite) Test_repository_Get() {
+// 	var (
+// 		id   = uuid.NewV4()
+// 		name = "test-name"
+// 	)
+//
+// 	s.mock.MatchExpectationsInOrder(false)
+// 	// s.mock.ExpectQuery(regexp.QuoteMeta(
+// 	// 	`SELECT * FROM "person" WHERE (id = $1)`)).
+// 	s.mock.ExpectQuery(`SELECT * FROM "person" WHERE (id = ?)`).
+// 		WithArgs(id.String()).
+// 		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(id.String(), name))
+//
+// 	res, err := s.repository.Get(id)
+//
+// 	require.NoError(s.T(), err)
+// 	require.Nil(s.T(), deep.Equal(&model.Person{ID: id, Name: name}, res))
+//
+// 	// // we make sure that all expectations were met
+// 	// if err := s.mock.ExpectationsWereMet(); err != nil {
+// 	// 	require.NoError(s.T(), err)
+// 	// }
+// }
 
 func (s *Suite) Test_repository_Create() {
 	var (
@@ -81,11 +85,15 @@ func (s *Suite) Test_repository_Create() {
 
 	s.mock.MatchExpectationsInOrder(false)
 	s.mock.ExpectBegin()
-	s.mock.ExpectQuery(regexp.QuoteMeta(
-		`INSERT INTO "person" ("id","name")
-			VALUES ($1,$2) RETURNING "person"."id"`)).
-		WithArgs(id, name).
-		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(id.String()))
+	// s.mock.ExpectQuery(regexp.QuoteMeta(
+	// 	`INSERT INTO "person" ("id","name")
+	// 		VALUES ($1,$2) RETURNING "person"."id"`)).
+
+	args := []driver.Value{id, name}
+
+	s.mock.ExpectExec(`INSERT INTO "person" ("id","name") VALUES (?,?)`).
+		WithArgs(args).WillReturnResult(sqlmock.NewResult(2, 1))
+	// WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(id.String()))
 	s.mock.ExpectCommit()
 
 	err := s.repository.Create(id, name)
@@ -95,4 +103,8 @@ func (s *Suite) Test_repository_Create() {
 	// if err := s.mock.ExpectationsWereMet(); err != nil {
 	// 	require.NoError(s.T(), err)
 	// }
+}
+
+func fixedFullRe(s string) string {
+	return fmt.Sprintf("^%s$", regexp.QuoteMeta(s))
 }
