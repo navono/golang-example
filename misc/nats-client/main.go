2dfb4c8e47514e2d0b63fa54317a16d3e674f0c7
navono <navono007@gmail.com>
2019-12-10 14:53:36 +0800

    feat: 增加 nats 的其他一些功能示例

--- misc/nats-client/main.go
+++ misc/nats-client/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/nats-io/nats.go"
 	"github.com/urfave/cli"
@@ -11,50 +13,75 @@ import (
 	"golang-example/cmd"
 )
 
-// go run main.go nats -s "nats://10.30.26.99:4222" aa bb
+// go run main.go nats publish -s "nats://10.30.26.99:4222" aa bb
 func init() {
 	cmd.Cmds = append(cmd.Cmds, cli.Command{
 		Name:    "nats",
 		Aliases: []string{"nats"},
 
 		Usage:    "Demonstration of nats client",
-		Action:   natsClientAction,
 		Category: "MQ",
-		Flags: []cli.Flag{
-			cli.StringFlag{
-				Name:     "s",
-				Usage:    "The nats server URLs (separated by comma)",
-				Required: true,
+		Subcommands: []cli.Command{
+			{
+				Name:   "publish",
+				Usage:  "basic nats publish",
+				Action: publishAction,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "s",
+						Usage:    "The nats server URLs (separated by comma)",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "creds",
+						Usage: "User Credentials File",
+					},
+				},
 			},
-			cli.StringFlag{
-				Name:  "creds",
-				Usage: "User Credentials File",
+			{
+				Name:   "reply",
+				Usage:  "basic nats reply",
+				Action: replyAction,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "s",
+						Usage:    "The nats server URLs (separated by comma)",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "q",
+						Value: "NATS-RPLY-22",
+						Usage: "Message queue",
+					},
+					cli.StringFlag{
+						Name:  "creds",
+						Usage: "User Credentials File",
+					},
+				},
 			},
-			// cli.BoolFlag{
-			// 	Name:        "h",
-			// 	Usage:       "Show help message",
-			// 	Destination: nil,
-			// },
+			{
+				Name:   "request",
+				Usage:  "basic nats request",
+				Action: requestAction,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "s",
+						Usage:    "The nats server URLs (separated by comma)",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:  "creds",
+						Usage: "User Credentials File",
+					},
+				},
+			},
 		},
 	})
 }
 
-func natsClientAction(c *cli.Context) error {
-	// var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
-	// var userCreds = flag.String("creds", "", "User Credentials File")
-	// var showHelp = flag.Bool("h", false, "Show help message")
-
+func publishAction(c *cli.Context) error {
 	urls := c.String("s")
 	userCreds := c.String("creds")
-	showHelp := c.Bool("h")
-
-	// log.SetFlags(0)
-	// flag.Usage = usage
-	// flag.Parse()
-
-	if showHelp {
-		showUsageAndExit(0)
-	}
 
 	if c.NArg() != 2 {
 		showUsageAndExit(1)
@@ -90,6 +117,92 @@ func natsClientAction(c *cli.Context) error {
 	return nil
 }
 
+func replyAction(c *cli.Context) error {
+	urls := c.String("s")
+	queueName := c.String("q")
+	userCreds := c.String("creds")
+
+	if c.NArg() != 2 {
+		showUsageAndExit(1)
+	}
+
+	// Connect Options.
+	opts := []nats.Option{nats.Name("NATS Sample Publisher")}
+
+	// Use UserCredentials
+	if userCreds != "" {
+		opts = append(opts, nats.UserCredentials(userCreds))
+	}
+
+	// Connect to NATS
+	nc, err := nats.Connect(urls, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	args := c.Args()
+	subj, reply, i := args[0], []byte(args[1]), 0
+
+	nc.QueueSubscribe(subj, queueName, func(msg *nats.Msg) {
+		i++
+		printMsg(msg, i)
+		msg.Respond([]byte(reply))
+	})
+	nc.Flush()
+
+	// Setup the interrupt handler to drain so we don't miss
+	// requests when scaling down.
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt)
+	<-s
+	log.Println()
+	log.Printf("Draining...")
+	nc.Drain()
+	log.Fatalf("Exiting")
+	return nil
+}
+
+func requestAction(c *cli.Context) error {
+	urls := c.String("s")
+	userCreds := c.String("creds")
+
+	if c.NArg() != 2 {
+		showUsageAndExit(1)
+	}
+
+	// Connect Options.
+	opts := []nats.Option{nats.Name("NATS Sample Publisher")}
+
+	// Use UserCredentials
+	if userCreds != "" {
+		opts = append(opts, nats.UserCredentials(userCreds))
+	}
+
+	// Connect to NATS
+	nc, err := nats.Connect(urls, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	args := c.Args()
+	subj, payload := args[0], []byte(args[1])
+
+	msg, err := nc.Request(subj, payload, 2*time.Second)
+	if err != nil {
+		if nc.LastError() != nil {
+			log.Fatalf("%v for request", nc.LastError())
+		}
+		log.Fatalf("%v for request", err)
+	}
+
+	log.Printf("Published [%s] : '%s'", subj, payload)
+	log.Printf("Received  [%v] : '%s'", msg.Subject, string(msg.Data))
+
+	return nil
+}
+
 func usage() {
 	log.Printf("Usage: nats-pub [-s server] [-creds file] <subject> <msg>\n")
 	flag.PrintDefaults()
@@ -98,4 +211,8 @@ func usage() {
 func showUsageAndExit(exitcode int) {
 	usage()
 	os.Exit(exitcode)
+}
+
+func printMsg(m *nats.Msg, i int) {
+	log.Printf("[#%d] Received on [%s]: '%s'\n", i, m.Subject, string(m.Data))
 }
