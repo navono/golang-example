d4d7d28513fb9037c2f72c2db87f7ae53f7deb02
pingqixing <pingqixing@supcon.com>
2019-12-27 08:31:53 +0800

    feat: pointer vs copy

--- blogs/pointer-copy/main.go
+++ blogs/pointer-copy/main.go
@@ -1,1 +1,46 @@
 package pointer_copy
+
+import (
+	"github.com/urfave/cli"
+
+	"golang-example/cmd"
+)
+
+func init() {
+	cmd.Cmds = append(cmd.Cmds, cli.Command{
+		Name:    "pointer",
+		Aliases: []string{"pointer"},
+
+		Usage:    "Demonstration of pointer and copy",
+		Action:   pointerAction,
+		Category: "Misc",
+	})
+}
+
+func pointerAction(c *cli.Context) error {
+	return nil
+}
+
+type (
+	S struct {
+		a, b, c int64
+		d, e, f string
+		g, h, i float64
+	}
+)
+
+func byCopy() S {
+	return S{
+		a: 1, b: 1, c: 1,
+		e: "foo", f: "foo",
+		g: 1.0, h: 1.0, i: 1.0,
+	}
+}
+
+func byPointer() *S {
+	return &S{
+		a: 1, b: 1, c: 1,
+		e: "foo", f: "foo",
+		g: 1.0, h: 1.0, i: 1.0,
+	}
+}
--- blogs/pointer-copy/main_test.go
+++ blogs/pointer-copy/main_test.go
@@ -1,1 +1,35 @@
 package pointer_copy
+
+import (
+	"fmt"
+	"testing"
+)
+
+// go get -u golang.org/x/tools/cmd/benchcmp
+
+// go test ./... -bench=BenchmarkMemoryHeap -trace=heapTrace.out -benchmem -run=^$ -count=10
+// go test ./... -bench=BenchmarkMemoryStack -trace=stackTrace.out -benchmem -run=^$ -count=10
+
+func BenchmarkMemoryStack(b *testing.B) {
+	var s S
+
+	for i := 0; i < b.N; i++ {
+		s = byCopy()
+	}
+
+	b.StopTimer()
+
+	_ = fmt.Sprintf("%v", s.a)
+}
+
+func BenchmarkMemoryHeap(b *testing.B) {
+	var s *S
+
+	for i := 0; i < b.N; i++ {
+		s = byPointer()
+	}
+
+	b.StopTimer()
+
+	_ = fmt.Sprintf("%v", s.a)
+}
